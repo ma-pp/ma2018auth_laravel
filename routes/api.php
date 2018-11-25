42d8659cecec635b77452d130e7673beb2dcedb8
firman <fsyah1544@gmail.com>
2018-11-25 20:04:29 +0700

    buat logOut

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use Illuminate\Http\Request;
 
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@login');
-Route::get('open', 'DataController@open');
+// Route::get('open', 'DataController@open');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::get('user', 'UserController@getAuthenticatedUser');
-    Route::get('closed', 'DataController@closed');
+    Route::get('user', 'UserController@getAuthenticatedUser');  
+    Route::get('logout', 'UserController@logout');
+    
 }); 
 
